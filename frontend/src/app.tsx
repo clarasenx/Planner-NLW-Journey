1fad6ff0f7542c740d53a62505f0899370d6ae48
Clara Helena <clarahelenasena@gmail.com>
2024-07-12 12:41:34 -0400

    add icons with lucide-react

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,23 @@
+import { MapPin, Calendar } from 'lucide-react'
+
 export default function App() {
   return (
     <div className='h-screen flex items-center justify-center'>
-      <div className='max-w-3xl w-full px-6 text-center space-y-10'>
-          <p className='text-zinc-500 text-lg'>Convide seus amigos e planeje sua própria viagem!</p>
-        <div className='h-16 bg-zinc-900 rounded-xl flex items-center shadow-shape'>
-          <input className='bg-transparent text-lg placeholder-zinc-400' type="text" placeholder='Para onde você vai?'/>
-          <input className='bg-transparent text-lg placeholder-zinc-400' type="text" placeholder='Quando?'/>
 
+      <div className='max-w-3xl w-full px-6 text-center space-y-10'>
+
+          <p className='text-zinc-500 text-lg'>Convide seus amigos e planeje sua própria viagem!</p>
+
+        <div className='h-16 bg-zinc-900 rounded-xl flex items-center shadow-shape'>
+
+          <div className='flex items-center gap-2 flex-1'>  
+            <MapPin className="size-5 text-zinc-400 flex gap-2"/>
+            <input className='bg-transparent text-lg placeholder-zinc-400' type="text" placeholder='Para onde você vai?'/>
+          </div>
+          <div className='flex items-center gap-2'>  
+            <Calendar className="size-5 text-zinc-400"/>
+            <input className='bg-transparent text-lg placeholder-zinc-400' type="text" placeholder='Quando?'/>
+          </div>
           <button>Continuar</button>
         </div>
           <p className='text-sm text-zinc-500'>Ao planejar sua viagem pela plann.er você automaticamente concorda <br />com nossos <a className='text-zinc-300 underline' href="#" target='_blank'>Termos de Uso</a> e <a className='text-zinc-300 underline' href="#" target="_blank">Políticas de privacidade</a>.</p>
