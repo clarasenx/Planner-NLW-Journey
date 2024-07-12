0bf302a9f89e58061c9b88114e8f069ed984772f
Clara Helena <clarahelenasena@gmail.com>
2024-07-12 13:55:43 -0400

    update app.tsx

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus } from 'lucide-react'
 import { useState } from 'react'
 
 export default function App() {
@@ -17,25 +17,44 @@ export default function App() {
           <p className='text-zinc-500 text-lg'>Convide seus amigos e planeje sua própria viagem!</p>
         </div>
         
-        <div className='flex items-center gap-3 px-5 h-16 bg-zinc-900 rounded-xl shadow-shape'>
+        <div className='space-y-4'>
+          <div className='flex items-center gap-3 px-5 h-16 bg-zinc-900 rounded-xl shadow-shape'>
 
-          <div className='flex items-center gap-2 flex-1'>  
-            <MapPin className="size-5 text-zinc-400 flex gap-2"/>
-            <input className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' type="text" placeholder='Para onde você vai?'/>
+            <div className='flex items-center gap-2 flex-1'>  
+              <MapPin className="size-5 text-zinc-400 flex gap-2"/>
+              <input className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' type="text" placeholder='Para onde você vai?'/>
+            </div>
+
+            <div className='flex items-center gap-2 min-w-'>  
+              <Calendar className="size-5 text-zinc-400"/>
+              <input className='bg-transparent text-lg placeholder-zinc-400 flex-1 w-40 outline-none' type="text" placeholder='Quando?'/>
+            </div>
+
+            <div className='w-px h-6 bg-zinc-800'/>
+
+              {isGuestInputOpen ? (
+                <button className='flex items-center gap-2  bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium'>Alterar local/data</button>
+              ) : (
+                <button onClick={openGuestInput} className='flex items-center gap-2  bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium'>Continuar <ArrowRight className='size-5'/></button>
+              )}
+
+          </div>
+        
+            {isGuestInputOpen && (
+              <div className='flex items-center gap-3 px-5 h-16 bg-zinc-900 rounded-xl shadow-shape'>
+
+            <div className='flex items-center gap-2 flex-1'>  
+              <UserRoundPlus className="size-5 text-zinc-400 flex gap-2"/>
+              <input className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' type="text" placeholder='Quem estará na viagem?'/>
+            </div>
+
+            <div className='w-px h-6 bg-zinc-800'/>
+            
+              <button className='flex items-center gap-2  bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium'>Confirmar viagem?<ArrowRight className='size-5'/></button>
           </div>
 
-          <div className='flex items-center gap-2 min-w-'>  
-            <Calendar className="size-5 text-zinc-400"/>
-            <input className='bg-transparent text-lg placeholder-zinc-400 flex-1 w-40 outline-none' type="text" placeholder='Quando?'/>
-          </div>
-          <div className='w-px h-6 bg-zinc-800'/>
-            <button onClick={openGuestInput} className='flex items-center gap-2  bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium'>Continuar <ArrowRight className='size-5'/></button>
-          </div>
-
-          {isGuestInputOpen ? (
-
-
-          ) : 'fechada'}
+            )}
+        </div>
 
           <p className='text-sm text-zinc-500'>Ao planejar sua viagem pela plann.er você automaticamente concorda <br />com nossos <a className='text-zinc-300 underline' href="#" target='_blank'>Termos de Uso</a> e <a className='text-zinc-300 underline' href="#" target="_blank">Políticas de privacidade</a>.</p>
       </div>
