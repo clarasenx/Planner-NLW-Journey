6190b09404387d1a84c9ffd0bb79e10e4f5644d9
Clara Helena <clarahelenasena@gmail.com>
2024-07-15 13:04:41 -0400

    Update app.tsx
    update app.tsx

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,6 +1,6 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import CreateTripPage from "./pages/create-trip"
-import { TripDetailsPage } from "./pages/trip-details"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTripPage from "./pages/create-trip";
+import { TripDetailsPage } from "./pages/trip-details";
 
 const router = createBrowserRouter([
   {
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     path: "/trips/:tripId",
     element: <TripDetailsPage />
   },
-])
+]);
 
 export function App() {
   return <RouterProvider router={router} />
