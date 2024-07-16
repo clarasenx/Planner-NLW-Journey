5b92b6162d2ab40285a0fb2e6244c9a9bb8b9bf4
Clara Helena <clarahelenasena@gmail.com>
2024-07-16 12:32:07 -0400

    add tailwind-variants and create Button Component

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { CreateActivityModal } from "./create-activity-modal"
 import { ImportantLinks } from './important-links'
 import { Activitylist } from './activity-list'
+import { Button } from '../components/button'
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
@@ -32,10 +33,10 @@ export function TripDetailsPage() {
 
           <div className='w-px h-6 bg-zinc-800'/>
 
-          <button className='bg-zinc-800 hover:bg-zinc-700 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2'>
-          Alternar local/data
+          <Button variant='secondary'>
+            Alternar local/data
             <Settings2 className='size-5'/>
-          </button>
+          </Button>
         </div>
       </div>
 
