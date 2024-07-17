5997fc492e9cd3e22b49de6804ffe65d4bd1587f
Clara Helena <clarahelenasena@gmail.com>
2024-07-17 11:59:06 -0400

    update index.tsx and guest-list (trip-details)

--- frontend/src/pages/trip-details/guest-list.tsx
+++ frontend/src/pages/trip-details/guest-list.tsx
@@ -1,27 +1,46 @@
 import { CircleCheck, CircleDashed, UserCog } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Button } from '../../components/button';
+import { api } from '../../lib/axios';
+
+interface Participant {
+  id: string
+  name: string | null
+  email: string
+  is_confirmed: boolean
+}
 
 export function GuestList() {
+  const { tripId } = useParams()
+  const [participants, setParticipants] = useState<Participant[] | []>()
+
+  useEffect(() => {
+    api.get(`/trips/%${tripId}/participants`).then(response => setParticipants(response.data.participants))
+  }, [tripId])
+
   return (
     <div className='space-y-6'>
       <h2 className='font-semibold text-xl'>Convidados</h2>
       <div className='space-y-5'>
         {/* links */}
-        <div className='flex items-center justify-between'>
-          <div className='space-y-1.5'>
-            <span className='block font-medium text-zinc-100'>Nome do usuário</span>
-            <span className='block text-sm text-zinc-400 truncate'>Email do usuário</span>
-          </div>
-          <CircleDashed className='size-5 text-zinc-400 shrink-0'/>
-        </div>
+        {participants?.map(participant => {
+          return (
+            <div key={participant.id} className='flex items-center justify-between'>
+              <div className='space-y-1.5'>
+                <span className='block font-medium text-zinc-100'>{participant.name}</span>
+                <span className='block text-sm text-zinc-400 truncate'>{participant.email}</span>
+              </div>
+              {participant.is_confirmed ? (
+                <CircleCheck className='size-5 text-zinc-400 shrink-0'/>
+              ) : (
+                <CircleDashed className='size-5 text-zinc-400 shrink-0'/>
+              )}
+            </div>
+          )
+        })}
               
-        <div className='flex items-center justify-between'>
-          <div className='space-y-1.5'>
-            <span className='block font-medium text-zinc-100'>Nome do usuário</span>
-             <span className='block text-sm text-zinc-400 truncate'>Email do usuário</span>
-          </div>
-          <CircleCheck className='size-5 text-lime-300 shrink-0'/>
-        </div>
+        
       </div>
 
       <Button size='full'>
--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,10 +1,21 @@
 import { Calendar, MapPin, Plus, Settings2 } from 'lucide-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from '../../components/button'
 import { Activitylist } from './activity-list'
 import { CreateActivityModal } from "./create-activity-modal"
 import { GuestList } from './guest-list'
 import { ImportantLinks } from './important-links'
+import { useParams } from 'react-router-dom'
+import { api } from '../../lib/axios'
+import { format } from 'date-fns'
+
+interface Trip {
+  id: string
+  destination: string
+  starts_at: string
+  ends_at: string
+  is_confirmed: boolean
+}
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
@@ -17,19 +28,30 @@ export function TripDetailsPage() {
     setIsCreateActivityModalOpen(false)
   }
 
+  const { tripId } = useParams()
+  const [trip, setTrip] = useState<Trip | undefined>()
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}`).then(response => setTrip(response.data.trip))
+  }, [tripId])
+
+   const displayedDate = trip
+  ? format(trip.starts_at, "d' de 'LLL").concat(' até ').concat(format(trip.ends_at, "d' de 'LLL"))
+  : null
+
   return (
     <div className='max-w-6xl px-6 py-10 mx-auto space-y-8'>
       {/* header com local/data e botao de config */}
       <div className='px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between'>
         <div className='flex items-center gap-2'>
           <MapPin className="size-5 text-zinc-400"/>
-          <span className='text-zinc-100'>Florianópolis, Brasil</span>
+          <span className='text-zinc-100'>{trip?.destination}</span>
         </div>
 
         <div className='flex items-center gap-5'>
           <div className='flex items-center gap-2'>
             <Calendar className="size-5 text-zinc-400"/>
-            <span className='text-zinc-100'>17 a 23 de Agosto</span>
+            <span className='text-zinc-100'>{displayedDate}</span>
           </div>
 
           <div className='w-px h-6 bg-zinc-800'/>
